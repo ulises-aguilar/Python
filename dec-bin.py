52be0829f7f3ad2c3c48b9ca0f5b1809ae6da036
ulises-aguilar <54950806+ulises-aguilar@users.noreply.github.com>
2020-10-16 09:43:32 -0700

    Add files via upload

---
+++ dec-bin.py
@@ -0,0 +1,22 @@
+print("DECIMAL", 0,"= BINARY", bin(0))
+print("DECIMAL", 0,"= BINARY", bin(0))
+print("DECIMAL", 1,"= BINARY", bin(1))
+print("DECIMAL", 2,"= BINARY", bin(2))
+print("DECIMAL", 3,"= BINARY", bin(3))
+print("DECIMAL", 4,"= BINARY", bin(4))
+print("DECIMAL", 5,"= BINARY", bin(5))
+print("DECIMAL", 6,"= BINARY", bin(6))
+print("DECIMAL", 7,"= BINARY", bin(7))
+print("DECIMAL", 8,"= BINARY", bin(8))
+print("DECIMAL", 9,"= BINARY", bin(9))
+print("DECIMAL", 10,"= BINARY", bin(10))
+print("DECIMAL", 11,"= BINARY", bin(11))
+print("DECIMAL", 12,"= BINARY", bin(12))
+print("DECIMAL", 13,"= BINARY", bin(13))
+print("DECIMAL", 14,"= BINARY", bin(14))
+print("DECIMAL", 15,"= BINARY", bin(15))
+print("DECIMAL", 16,"= BINARY", bin(16))
+'''
+2^7 2^6 2^5 2^4 2^3 2^2 2^1 2^0
+128 64  32  16  8    4   2   1
+'''
